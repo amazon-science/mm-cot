c628899a4560ecc44826121c9ce2cef4f30e36f1
Zhuosheng Zhang <719579371@qq.com>
2023-03-23 11:59:37 +0800

    add extract_features.py

--- README.md
+++ README.md
@@ -25,6 +25,27 @@ https://github.com/lupantech/ScienceQA/tree/main/data
 
 Download the extracted vision features from [vision_features](https://drive.google.com/file/d/13B0hc_F_45-UlqPLKSgRz-ALtFQ8kIJr/view?usp=share_link) and unzip the files under `vision_features`
 
+## Extract Features (optional)
+Download the image files from [Google Drive](https://drive.google.com/drive/folders/1w8imCXWYn2LxajmGeGH_g5DaL2rabHev?usp=sharing) and unzip all the images (train, dev, test) in the same folder (). The structure should be:
+
+```
+images
+├── 1
+│   └── image.png
+├── 2
+│   └── image.png
+├── 3
+│   └── image.png
+├── 5
+│   └── image.png
+├── 7
+│   └── image.png
+```
+
+Run ```extract_features.py --data_root images --output_dir vision_features --img_type detr```
+
+If you hope to use your own images, please structure those images in the way above, or modify the script ```extract_features.py```.
+
 ## Instructions
 
 ### Training 
@@ -90,4 +111,4 @@ This project is licensed under the Apache-2.0 License.
 
 Part of our codes are adapted from [ScienceQA](https://github.com/lupantech/ScienceQA) and [Transformers](https://github.com/huggingface/transformers).
 
-We thank [Pan Lu](https://lupantech.github.io/) for providing parameter size for ScienceQA baselines.
+We thank Pan Lu for providing parameter size for ScienceQA baselines.
---
+++ extract_features.py
@@ -0,0 +1,74 @@
+import torch
+from PIL import Image
+import torchvision.transforms as T
+import timm
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+import os
+import argparse
+import json
+from tqdm import tqdm
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root', type=str, default='images')
+    parser.add_argument('--output_dir', type=str, default='vision_features')
+    parser.add_argument('--img_type', type=str, default="vit", choices=['detr', 'vit'], help='type of image features')
+    args = parser.parse_args()
+    return args
+
+def extract_features(img_type, input_image):
+    if img_type == "vit":
+        config = resolve_data_config({}, model=vit_model)
+        transform = create_transform(**config)
+        with torch.no_grad():
+            img = Image.open(input_image).convert("RGB")
+            input = transform(img).unsqueeze(0)
+            out = vit_model.forward_features(input)
+            patch_feature, pooled_feature = out
+            patch_feature = patch_feature.squeeze(0)
+            concate_feature = torch.cat([pooled_feature, patch_feature], dim=0)
+            feature = concate_feature.unsqueeze(0)
+        return feature
+    
+    elif img_type == "detr":
+        transform = T.Compose([
+            T.Resize(224),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        with torch.no_grad():
+            img = Image.open(input_image).convert("RGB")
+            input = transform(img).unsqueeze(0)
+            feature = detr_model(input)[-1]
+        return feature
+
+if __name__ == '__main__':
+    args = parse_args()
+    print("args",args)
+    all_images = os.listdir(args.data_root)
+    tmp = []
+    name_map = {}
+    all_images.sort(key=lambda x:int(x))
+    print(len(all_images))
+    if args.img_type == "vit":
+        vit_model = timm.create_model("vit_base_patch16_384", pretrained=True, num_classes=0)
+        vit_model.eval()
+    elif args.img_type == "detr":
+        detr_model = torch.hub.load('cooelf/detr', 'detr_resnet101_dc5', pretrained=True)
+        detr_model.eval()
+    for idx, image in enumerate(tqdm(all_images)):
+        if idx % 100 == 0: print(idx)
+        if os.path.exists(os.path.join(args.data_root, image, "image.png")):
+            curr_dir = os.path.join(args.data_root, image, "image.png")
+        else:
+            curr_dir = os.path.join(args.data_root, image, "choice_0.png")
+        feature = extract_features(args.img_type, curr_dir)
+        tmp.append(feature.detach().cpu())
+        name_map[str(image)] = idx
+    
+    res = torch.cat(tmp).cpu()
+    print(res.shape)
+    torch.save(res, os.path.join(args.output_dir, args.img_type +'.pth'))
+    with open(os.path.join(args.output_dir, 'name_map.json'), 'w') as outfile:
+        json.dump(name_map, outfile)
