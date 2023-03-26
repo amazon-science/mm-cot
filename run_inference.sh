2047fbb0f1aa6667e3605c70fca9fc007ccbcd75
Zhuosheng Zhang <719579371@qq.com>
2023-03-26 09:30:25 +0800

    update shell

--- run_inference.sh
+++ run_inference.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0,1 python main.py \
     --model allenai/unifiedqa-t5-base \
     --user_msg rationale --img_type detr \
     --bs 8 --eval_bs 4 --eval_acc 10 --output_len 512 \
-    --final_eval --use_caption --prompt_format QCM-E \
+    --final_eval --prompt_format QCM-E \
     --evaluate_dir models/rationale
 
 # answer inference
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=0,1 python main.py \
     --model allenai/unifiedqa-t5-base \
     --user_msg answer --img_type detr \
     --bs 8 --eval_bs 4 --eval_acc 10 --output_len 64 \
-    --final_eval --use_caption --prompt_format QCMG-A \
+    --final_eval --prompt_format QCMG-A \
     --eval_le models/rationale/predictions_ans_eval.json \
     --test_le models/rationale/predictions_ans_test.json \
     --evaluate_dir models/answer
